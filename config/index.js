c188818081c46c871faa432421a551346a760b0c
AleGarAlon <126183093+AleGarAlon@users.noreply.github.com>
2025-03-26 23:01:00 +0100

    Update index.js

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
  app.use(
     cors({
-      origin: "*",
+      origin: [FRONTEND_URL],
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
       allowedHeaders: ["Content-Type", "Authorization"],
